af2e6614d515993788679325f8b06749e432a027
dovadallas661-ux <dovadallas661@gmail.com>
2026-08-04 21:37:34 +0100

    Implement random price and timestamp in getStockData

--- fakeStockAPI.js
+++ fakeStockAPI.js
@@ -1,8 +1,8 @@
 function getStockData() {
     return {
-        name: 'QtechAI',
-        sym: 'QTA',
-        price: /* return a random number between 0 and 3 to two decimal places */, 
-        time: /* return a timestamp in this format: hh:mm:ss */
+        name: "QtechAI",
+        sym: "QTA",
+        price: (Math.random() * 3).toFixed(2),
+        time: new Date().toLocaleTimeString()
     }
 }
